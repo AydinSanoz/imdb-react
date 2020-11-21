ccc65122cdcb0ffaf27963a6b3824fa7da61ea24
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-21 13:28:22 +0300

    reducer and states deleted

--- src/context/States.js
+++
@@ -1,8 +0,0 @@
-import {useState} from 'react';
-
-export function States() {
-  const [movieList, setMovieList] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState('');
-  const [inputValue, setInputValue] = useState('');
-  const [searchText, setSearchText] = useState('');
-}
