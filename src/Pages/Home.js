0462ea96fa3a03dcd7ee62354ce0b8a9a40bb423
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-02 16:05:27 +0300

    pagination added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import { StyledMainContainer } from './Styles';
 import { MovieList, TvList, PlayingList, VideoList } from './Sections';
 import { ListContext, SearchContext } from '../context';
+import Pagination from '@material-ui/lab/Pagination';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
 
 const myApi = '64f9385d47c970d860ccd6c147841242';
 const baseImgUrl = 'https://image.tmdb.org/t/p/w500';
@@ -12,79 +15,114 @@ const baseVideoUrl = 'https://api.themoviedb.org/3/movie/200/videos';
 const baseNowPlayingUrl = 'https://api.themoviedb.org/3/movie/now_playing';
 const baseYouTubeUrl = 'https://www.youtube.com/embed';
 
+const useStyles = makeStyles((theme) => ({
+	paginationBar: {
+		display: 'flex',
+		justifyContent: 'center',
+		backgroundColor: 'brown',
+		position : "fixed",
+		zIndex : 1
+	},
+	root: {
+		'& > * + *': {
+			marginTop: theme.spacing(2),
+		},
+	},
+}));
+
 export const Home = () => {
-  const [movieList, setMovieList] = useState([]);
-  const [tvList, setTvList] = useState([]);
-  const [videoList, setVideoList] = useState([]);
-  const [nowPlayingList, setNowPlayingList] = useState([]);
+	const [currentPage, setCurrentPage] = useState(10);
+	const [movieList, setMovieList] = useState([]);
+	const [tvList, setTvList] = useState([]);
+	const [videoList, setVideoList] = useState([]);
+	const [nowPlayingList, setNowPlayingList] = useState([]);
 
-  const { inputVal, query } = useContext(SearchContext);
-  console.log('Home -> videoList', videoList);
+	const { inputVal, query } = useContext(SearchContext);
+	const classes = useStyles();
 
-  useEffect(() => {
-    axios
-      .get(baseMovieUrl, {
-        params: {
-          api_key: myApi,
-          query: inputVal || 'a',
-          page: 1,
-        },
-      })
-      .then((res) => setMovieList(res.data.results))
-      .catch((err) => console.log(err))
-      .finally();
-  }, [inputVal]);
+	console.log('Home -> videoList', videoList);
 
-  useEffect(() => {
-    axios
-      .get(baseTvUrl, {
-        params: {
-          api_key: myApi,
-          query: inputVal || 'a',
-        },
-      })
-      .then((res) => setTvList(res.data.results));
-  }, [inputVal]);
+	useEffect(() => {
+		axios
+			.get(baseMovieUrl, {
+				params: {
+					api_key: myApi,
+					query: inputVal || 'a',
+					page: currentPage,
+				},
+			})
+			.then((res) => {
+				console.log('movielist', res.data.results);
+				setMovieList(res.data.results);
+			})
+			.catch((err) => console.log(err))
+			.finally();
+	}, [inputVal, currentPage]);
 
-  useEffect(() => {
-    axios
-      .get(baseNowPlayingUrl, {
-        params: {
-          api_key: myApi,
-          page: 1,
-        },
-      })
-      .then((res) => setNowPlayingList(res.data.results));
-  }, [inputVal]);
+	useEffect(() => {
+		axios
+			.get(baseTvUrl, {
+				params: {
+					api_key: myApi,
+					query: inputVal || 'a',
+				},
+			})
+			.then((res) => setTvList(res.data.results));
+	}, [inputVal]);
 
-  useEffect(() => {
-    axios
-      .get(baseVideoUrl, {
-        params: {
-          api_key: myApi,
-        },
-      })
-      .then((res) => setVideoList(res.data.results))
-      .catch()
-      .finally();
-  }, [inputVal]);
+	useEffect(() => {
+		axios
+			.get(baseNowPlayingUrl, {
+				params: {
+					api_key: myApi,
+					page: { currentPage },
+				},
+			})
+			.then((res) => setNowPlayingList(res.data.results));
+	}, [inputVal, currentPage]);
 
-  return (
-    <StyledMainContainer>
-      <ListContext.Provider
-        value={{
-          movieList,
-          baseImgUrl,
-          tvList,
-          nowPlayingList,
-          videoList,
-          baseYouTubeUrl,
-        }}
-      >
-        <MovieList />
-        <TvList />
-        <PlayingList />
-      </ListContext.Provider>
-    </StyledMainContainer>
-  );
+	useEffect(() => {
+		axios
+			.get(baseVideoUrl, {
+				params: {
+					api_key: myApi,
+				},
+			})
+			.then((res) => setVideoList(res.data.results))
+			.catch()
+			.finally();
+	}, [inputVal]);
+
+	const handleChange = (event, value) => {
+		setCurrentPage(value);
+	};
+	return (
+		<StyledMainContainer>
+			<Container className={classes.paginationBar}>
+				<Pagination
+					count={50}
+					siblingCount={1}
+					boundaryCount={1}
+					defaultPage={6}
+					color="primary"
+					onChange={handleChange}
+				/>
+			</Container>
+			<div style = {{height : "2rem"}}></div>
+			<ListContext.Provider
+				value={{
+					movieList,
+					baseImgUrl,
+					tvList,
+					nowPlayingList,
+					videoList,
+					baseYouTubeUrl,
+				}}
+			>
+				<MovieList />
+				<TvList />
+				<PlayingList />
+			</ListContext.Provider>
+		</StyledMainContainer>
+	);
 };
